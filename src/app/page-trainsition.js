208ea8b59b0b5c02270104dca0de5b18302b2d4b
techyVS <victorsky90@gmail.com>
2025-02-08 18:42:35 +0100

    added a product image to the product image's card as well as working to set up page transitions

---
+++ src/app/page-trainsition.js
@@ -0,0 +1,24 @@
+const runTransitions = () => {
+	window.addEventListener('DOMContentLoaded', () => {
+		const elemToWatch = document.querySelector('.intersecting-element');
+
+		const options = {
+			// root: document.querySelector('.product-section'),
+			rootMargin: '-150px',
+			threshold: 1,
+		};
+
+		const intersectionObserver = new IntersectionObserver((entries) => {
+			entries.forEach((entry) => {
+				if (entry.isIntersecting) {
+					console.log(`${entry.target} has been observed`);
+					entry.target.style.backgroundColor = 'red';
+				}
+			});
+		}, options);
+
+		intersectionObserver.observe(elemToWatch);
+	});
+};
+
+export { runTransitions };
--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import { runAlertService } from './app/app';
 import { loadNavbar } from './app/navbar';
 import { loadFooter } from './app/footer';
 import { undoFooterMargin } from './app/app';
+import { runTransitions } from './app/page-trainsition';
 
 loadNavbar();
 loadFooter();
 undoFooterMargin();
 // runAlertService(alertMessage);
+
+runTransitions();
